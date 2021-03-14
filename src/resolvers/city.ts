f0eaa31d3f447f3b0d751548587bb5b63e11220a
Andrii Malykhin <andrii.malykhin@tech.better.team>
2021-03-14 18:51:25 +0100

    migrate city endpoint to google api

--- src/resolvers/city.ts
+++ src/resolvers/city.ts
@@ -3,27 +3,30 @@ import { RESTDataSource, RequestOptions } from 'apollo-datasource-rest';
 interface ICity {
   name: string;
   country: string;
-  c: string;
-  zmw: string;
-  tz: string;
-  tzs: string;
-  l: string;
-  ll: string;
-  lat: string;
-  lon: string;
 }
 
+/* eslint-disable camelcase */
+interface IGoogleCity {
+  description: string;
+  structured_formatting: {
+    main_text: string;
+    main_text_matched_substrings: any;
+    secondary_text: string;
+  },
+  terms: { offset: number, value: string; }[],
+  types: string[];
+}
+/* eslint-enable camelcase */
+
 interface ICitiesResponse {
-  Results: ICity[];
+  predictions: IGoogleCity[];
 }
 
 export default class CityResolver extends RESTDataSource {
   public baseURL = process.env.CITY_ENDPOINT;
 
   protected willSendRequest(request: RequestOptions) {
-    request.headers.set('content-type', 'application/octet-stream');
-    request.headers.set('x-rapidapi-host', this.context.CITY_HOST);
-    request.headers.set('x-rapidapi-key', this.context.CITY_KEY);
+    request.params.set('key', this.context.GOOGLE_API_KEY);
   }
 
   public async getCity(
@@ -33,14 +36,19 @@ export default class CityResolver extends RESTDataSource {
       const response: ICitiesResponse = await this.get(
         '',
         {
-          location: cityName,
+          input: cityName,
         },
       );
 
-      return response.Results.map((item: ICity) => {
-        const [name, country] = item.name.split(/,\s/);
+      return response.predictions.map((item: IGoogleCity) => {
+        const temsCount = item.terms.length;
+        const name = item.terms[0].value;
+        const country = item.terms[temsCount - 1].value;
 
-        return { ...item, name, country };
+        return {
+          name,
+          country,
+        };
       });
     } catch (error) {
       // eslint-disable-next-line no-console
--- src/schema.ts
+++ src/schema.ts
@@ -4,14 +4,6 @@ export default gql`
 type City {
     name: String!
     country: String!
-    c: String!
-    zmw: String!
-    tz: String!
-    tzs: String!
-    l: String!
-    ll: String!
-    lat: String!
-    lon: String!
 }
 type Query {
     city(name: String): [City]
