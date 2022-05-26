eece2c46437ea97782acd3ad3d9b1cf0b4dfc1c6
Andrii Malykhin <malykhin.andrey@gmail.com>
2022-05-26 13:18:56 +0200

    migrate to apollo v3. update dependencies

--- src/apolloServer.ts
+++ src/apolloServer.ts
@@ -1,4 +1,11 @@
 import { ApolloServer } from 'apollo-server-express';
+import {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageDisabled,
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} from 'apollo-server-core';
+import { Server } from 'http';
+
 import typeDefs from './schema';
 import CityResolver from './resolvers/city';
 
@@ -9,17 +16,22 @@ const resolvers = {
   },
 };
 
-export default new ApolloServer({
+export const createServer = (httpServer: Server) => new ApolloServer({
   typeDefs,
   resolvers,
-  introspection: process.env.NODE_ENV === 'development' || Boolean(process.env.DEBUG_MODE),
-  playground: process.env.NODE_ENV === 'development' || Boolean(process.env.DEBUG_MODE),
+  introspection:
+      process.env.NODE_ENV === 'development' || Boolean(process.env.DEBUG_MODE),
+  csrfPrevention: true,
+  plugins: [
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+    process.env.NODE_ENV === 'development' || Boolean(process.env.DEBUG_MODE)
+      ? ApolloServerPluginLandingPageGraphQLPlayground()
+      : ApolloServerPluginLandingPageDisabled(),
+  ],
   dataSources: () => ({
     city: new CityResolver(),
   }),
-  context: () => (
-    {
-      GOOGLE_API_KEY: process.env.GOOGLE_API_KEY as string,
-    }
-  ),
+  context: () => ({
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY as string,
+  }),
 });
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import http from 'http';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import apolloServer from './apolloServer';
+import { createServer } from './apolloServer';
 
 const app = express();
+const httpServer = http.createServer(app);
 
 if (process.env.NODE_ENV === 'development') {
   dotenv.config();
@@ -13,14 +15,18 @@ app.use(cors());
 
 app.get('/', (_req, res) => {
   res.send(
-    `weatherApi server is running in ${process.env.NODE_ENV
-      || 'development'} mode`,
+    `weatherApi server is running in ${
+      process.env.NODE_ENV || 'development'
+    } mode`,
   );
 });
+const apolloServer = createServer(httpServer);
 
-apolloServer.applyMiddleware({ app });
+apolloServer.start().then(() => {
+  apolloServer.applyMiddleware({ app, path: '/' });
 
-const port = process.env.PORT || 5000;
+  const port = process.env.PORT || 5000;
 
-// eslint-disable-next-line no-console
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+  // eslint-disable-next-line no-console
+  httpServer.listen(port, () => console.log(`App listening on port ${port}!`));
+});
